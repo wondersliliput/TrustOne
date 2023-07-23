060a24d33df9e0aff97e25e8c7a55ba0a696df41
Peyman <107207816+wondersliliput@users.noreply.github.com>
2023-07-23 10:57:42 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x471676b80cb8e178ea61fc0f3b5a63cd716e45af";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
